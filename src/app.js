bc1ea988e0751e4e6bc2b573ad0b8e0cc6de362e
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-10-18 18:45:52 +0000

    borro cosas innecesarias

--- src/app.js
+++ src/app.js
@@ -1,27 +1,19 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-window.onload = function() {
-  //write your code here
-
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-
-  const randVal = numWords => {
-    return Math.floor(Math.random() * numWords);
-  };
-
-  let pronounShow = pronoun[randVal(pronoun.length)];
-  let adjShow = adj[randVal(adj.length)];
-  let nounShow = noun[randVal(noun.length)];
-  let print = `${pronounShow}${adjShow}${nounShow}.com`;
-
-  console.log(print);
-
-  document.querySelector("#excuse").innerHTML = print;
+const randVal = numWords => {
+  return Math.floor(Math.random() * numWords);
 };
+
+let pronounShow = pronoun[randVal(pronoun.length)];
+let adjShow = adj[randVal(adj.length)];
+let nounShow = noun[randVal(noun.length)];
+let print = `${pronounShow}${adjShow}${nounShow}.com`;
+
+console.log(print);
+
+document.querySelector("#excuse").innerHTML = print;
