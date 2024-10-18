34fd11e3fe312feeaf0ba08f0ea3ba3b7f36de6b
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-10-18 18:37:58 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  const randVal = numWords => {
+    return Math.floor(Math.random() * numWords);
+  };
+
+  let pronounShow = pronoun[randVal(pronoun.length)];
+  let adjShow = adj[randVal(adj.length)];
+  let nounShow = noun[randVal(noun.length)];
+  let print = `${pronounShow}${adjShow}${nounShow}.com`;
+
+  console.log(print);
+
+  document.querySelector("#excuse").innerHTML = print;
 };
